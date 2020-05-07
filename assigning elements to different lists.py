b19629bf1da5c62e37ae0e8da8d6c087ec7e7a96
Shreya0306 <55764751+Shreya0306@users.noreply.github.com>
2020-05-07 16:04:05 +0530

    Add files via upload

---
+++ assigning elements to different lists.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+fruits=["apple","orange","banana","cherry"]
+num=[4 for i in range (20)]
+mixed=["sreya",1,"b",{},(9,10)]
+print(fruits)
+print(num)
+print(mixed)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ positive numbers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+list1=[12,-7,5,64,-14]
+for n1 in list1:
+    if n1>=0:
+        print(n1,end=" ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
