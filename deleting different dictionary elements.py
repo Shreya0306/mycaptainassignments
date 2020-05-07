9408a417e603afb16f34007305a1a7d1535334cd
Shreya0306 <55764751+Shreya0306@users.noreply.github.com>
2020-05-07 17:16:10 +0530

    Add files via upload

---
+++ deleting different dictionary elements.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+cubes={1:1,2:8,3:27,4:64,5:125,6:126}
+print(cubes.pop(3))
+print(cubes)
+print(cubes.popitem())
+print(cubes)
+del cubes[1]
+print(cubes)
+del cubes[4]
+print(cubes)
+
+
+# In[ ]:
+
+
+
+
